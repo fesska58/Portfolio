1d291e384e84e0cbbb8a3a015a8449947a674077
fesska58 <71726333+fesska58@users.noreply.github.com>
2020-12-16 16:47:51 +0300

    Черновой,почти готов

--- main.js
+++ main.js
@@ -1,30 +1,41 @@
-let cards = document.querySelectorAll('.grid-card');
-let modal = document.querySelector('.modal-container');
-let btn = document.querySelector('.btn-close');
-let card = null;
-for (let i = 0; i < cards.length; i++) {
-    let card = cards[i];
 
-    card.addEventListener('click', function (e) {
-        modal.classList.add('show');
-    });
+function app() {
+    let buttons = document.querySelectorAll('.button');
+    let cards = document.querySelectorAll('.grid-card');
+    let modal = document.querySelector('.modal-container');
+    let btn = document.querySelector('.btn-close');
+    let card = null;
+    for (let i = 0; i < cards.length; i++) {
+        let card = cards[i];
 
-    btn.addEventListener('click', function () {
-        modal.classList.remove('show');
-    });
+        card.addEventListener('click', function (e) {
+            modal.classList.add('show');
+        });
+
+        btn.addEventListener('click', function () {
+            modal.classList.remove('show');
+        });
+    }
+
+    function filter(category, items) {
+        items.forEach((item) => {
+            let isItemFiltered = !item.classList.contains(category)
+            let isShowAll = category.toLowerCase() === 'all'
+            if (isItemFiltered && !isShowAll) {
+                item.classList.add('anime')
+            } else {
+                item.classList.remove('anime')
+            }
+        })
+    }
+
+    buttons.forEach((button) => {
+        button.addEventListener('click', function () {
+            let current = button.dataset.filter
+            filter(current, cards)
+        })
+    })
+
 }
 
-
-// modal.onclick = function () {
-//     modalContainer.style.display = "block";
-// }
-
-// btn.onclick = function () {
-//     modalContainer.style.display = "none";
-// }
-
-// let div = document.getElementsByClassName('grid-card')[0];
-
-// div.addEventListener('click', function (event) {
-//     alert('Hi!');
-// });
+app()
